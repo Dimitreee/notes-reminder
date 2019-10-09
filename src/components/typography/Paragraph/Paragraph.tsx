30cb2afb83136f1ca34c4935d2bd4768e6ff642b
Дмитрий Данев <d.danev@superjob.ru>
2019-10-09 21:12:12 +0300

    change layout styles; change paragraph styles

--- src/components/typography/Paragraph/Paragraph.tsx
+++ src/components/typography/Paragraph/Paragraph.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
+import classNames from 'classnames';
 import { Text } from '@blueprintjs/core';
 
-const Paragraph: React.FC = ({ children }) => (
-    <Text tagName="p" className='bp3-text-large'>{ children }</Text>
-);
+import s from './Paragraph.module.css';
+
+const Paragraph: React.FC = ({ children }) => {
+    const className = classNames(s.root, 'bp3-text-large');
+
+    return (
+        <Text tagName="p" className={className}>{ children }</Text>
+    );
+};
 
 export default Paragraph
